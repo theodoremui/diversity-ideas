40464f9cde2dbbee606e3e400cfebc53711b73f9
Phil Mui <pmui@salesforce.com>
2025-01-02 18:53:11 -0800

    removing unused folders

--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from diversity-ideas!")
-
-
-if __name__ == "__main__":
-    main()
--- scripts/run-berkeley.sh
+++
@@ -1,9 +0,0 @@
-# Package: 'unbuffer'
-#     - may need to install: "brew install expect" for unbuffer
-# 
-# Package: 'watch'
-#     - may need to install: "brew install watch"
-#     - to watch growth of log:
-#           `watch -n 5 "ls -la ./berkeley-opinion-LATEST.log"`
-#
-unbuffer python ./src/berkeley2parquet.py 200, 612, 3 2>&1 | tee -a ./data/berkeley-opinion-LATEST.log
